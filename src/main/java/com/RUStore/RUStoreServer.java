b2536e3cd9683875a4ffdae36d72e603b9c54d6e
mz487 <mz487@github.com>
2022-09-29 20:36:07 -0400

    filled the RUStoreServer code. re-examine every single function no matter how small to do a check of theory and syntax and logic

--- src/main/java/com/RUStore/RUStoreServer.java
+++ src/main/java/com/RUStore/RUStoreServer.java
@@ -1,17 +1,64 @@
 package com.RUStore;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.RUStore.message.RequestType.*;
+import static com.RUStore.message.ResponseMessageBuilder.*;
+import static com.RUStore.message.UtilTools.*;
+
 /* any necessary Java packages here */
 
 public class RUStoreServer {
 
 	/* any necessary class members here */
+	
+	private static Map<String, byte[]> objStore = new HashMap<>();
 
 	/* any necessary helper methods here */
+	private static void sendAllKeys(DataOutputStream out) throws IOException {
+		String[] keys = (String[]) objStore.keySet().toArray();
+		sendPacket(out, getKeysSuccessResponse(keys));
+	}
+	private static void deleteEntry(DataOutputStream out, DataInputStream in) throws IOException {
+		String key = new String(readPacket(in, in.readInt()), charSet);
+		byte[] data = objStore.remove(key);
+		if (data == null)
+			sendPacket(out, keyNFResponse());
+		else
+			sendPacket(out, delSuccessResponse());
+	}
+	private static void getDataFulfiller(DataOutputStream out, DataInputStream in) throws IOException {
+		String key = new String(readPacket(in, in.readInt()), charSet);
+		byte[] data = objStore.get(key);
+		if (data == null)
+			sendPacket(out, keyNFResponse());
+		else
+			sendPacket(out, getDataSuccessResponse(data));
+	}
+	private static void putData(DataOutputStream out, DataInputStream in) throws IOException {
+		String key = new String(readPacket(in, in.readInt()), charSet);
+		byte[] data = readPacket(in, in.readInt());
+		if (objStore.containsKey(key)) {
+			sendPacket(out, putDupeResponse());
+		} else {
+			objStore.put(key, data);
+			sendPacket(out, putSuccessResponse());
+		}
+	}
 
 	/**
 	 * RUObjectServer Main(). Note: Accepts one argument -> port number
+	 * @throws IOException 
 	 */
-	public static void main(String args[]){
+	public static void main(String args[]) throws IOException{
 
 		// Check if at least one argument that is potentially a port number
 		if(args.length != 1) {
@@ -22,9 +69,49 @@ public class RUStoreServer {
 		// Try and parse port # from argument
 		int port = Integer.parseInt(args[0]);
 
-
 		// Implement here //
-
+		try (ServerSocket server = new ServerSocket(port);) {
+			while (true) {
+				try (
+						Socket clientSocket = server.accept();
+						DataInputStream in = new DataInputStream(
+								new BufferedInputStream(
+										clientSocket.getInputStream()
+										)
+								);
+						DataOutputStream out = new DataOutputStream(
+								new BufferedOutputStream(
+										clientSocket.getOutputStream()
+										)
+								);
+				) {
+					boolean run = true;
+					while (run) {
+						byte requestType = in.readByte();
+						switch (requestType) {
+							case DISCONNECT_REQUEST :
+								run = false;
+								break;
+							case GET_KEYS_REQUEST :
+								sendAllKeys(out);
+								break;
+							case DEL_DATA_REQUEST :
+								deleteEntry(out, in);
+								break;
+							case GET_DATA_REQUEST :
+								getDataFulfiller(out, in);
+								break;
+							case PUT_FILE_REQUEST :
+								putData(out, in);
+								break;
+							case PUT_OBJ_REQUEST :
+								putData(out, in);
+								break;
+						}
+					}
+				}
+			}
+		}
 
 	}
 
